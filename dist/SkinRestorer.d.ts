bbbcdbe8878cd5c6ce6404c766661d7f43c81cc9
Mirko <MirkoCerasi@hotmail.com>
2025-02-16 15:40:07 +0100

    removing dist

--- dist/SkinRestorer.d.ts
+++
@@ -1,7 +0,0 @@
-import { DatabaseProvider } from './types';
-import Player from './Player';
-export default class SkinRestorer {
-    private dbProvider;
-    constructor(dbProvider: DatabaseProvider);
-    getPlayer(playerName: string): Promise<Player>;
-}
